7f02ca911a35705b060c08e8a678753d2abb7d21
Bogdan Jovanovic <jbogdan1998@hotmail.com>
2023-05-17 18:42:44 +0200

    Setup Selenium & WebDriverManager

---
+++ src/main/java/com/jovanovicbogdan/Main.java
@@ -0,0 +1,31 @@
+package com.jovanovicbogdan;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        String title = driver.getTitle();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+
+        driver.quit();
+    }
+}
